23f749b7e8fa3cc307ca81cc24b0596b059a0ebb
chw-owo <gpdnjs2116@naver.com>
2023-04-19 12:56:56 +0900

    edit

--- Assignment19_ProcessControl/Assignment19_ProcessControl/Assignment19_ProcessControl.cpp
+++ Assignment19_ProcessControl/Assignment19_ProcessControl/Assignment19_ProcessControl.cpp
@@ -8,56 +8,60 @@ void ProcessControl()
     scanf_s("%d", & processID);
     HANDLE hProcess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, processID);
     
-    int originData;
+    int iOriginData;
     printf("\n찾고자 하는 데이터를 입력하세요: ");
-    scanf_s("%d", &originData);
+    scanf_s("%d", &iOriginData);
+    char chOriginData[4];
+    int flag = 0x000000ff;
+
+    for (int i = 0; i < 4; i++)
+    {
+        chOriginData[3 - i] = ((iOriginData >> (i * 8)) & flag);
+    }
 
     SYSTEM_INFO SystemInfo;
     GetSystemInfo(&SystemInfo);
     char* pMemoryPos = (char*)SystemInfo.lpMinimumApplicationAddress;
-    char* pMemoryPosOld = pMemoryPos;
+    char* pMemoryPosOld = 0;
     char* pTargetPos = nullptr;
 
-    while (pMemoryPos >= pMemoryPosOld)
+    printf("\n데이터를 탐색합니다");
+    while (pMemoryPos > pMemoryPosOld)
     {
-        printf("%u\n", pMemoryPos);
-
+        printf("\n%u ~ %u", pMemoryPosOld, pMemoryPos);
         MEMORY_BASIC_INFORMATION MemoryBasicInfo;
         SIZE_T ret = VirtualQueryEx(hProcess, pMemoryPos,
             &MemoryBasicInfo, sizeof(MemoryBasicInfo));
 
         int size = MemoryBasicInfo.RegionSize;
+
         if (MemoryBasicInfo.Type == MEM_PRIVATE &&
             MemoryBasicInfo.State == MEM_COMMIT)
         {
-            int counted = 0;
-            char buf[5];
-            char* ptr = pMemoryPos;
-           
-            while (counted < size)
+            char* buffer = (char*) malloc(size + 1);
+            memset(buffer, 0, size + 1);
+            ReadProcessMemory(hProcess, pMemoryPos, buffer, size, NULL);
+            buffer[size] = '\0';
+            pTargetPos = strstr(buffer, chOriginData);
+
+            if (pTargetPos != nullptr)
             {
-                memset(buf, 0, 4);
-                ReadProcessMemory(hProcess, ptr, buf, 4, NULL);
-                buf[4] = '\0';
-                if (*buf == (char)originData) // To-do !!!!!!
-                {
-                    int newData;
-                    pTargetPos = (char*)((int)MemoryBasicInfo.BaseAddress + counted);
-                    printf("\n데이터를 찾았습니다! 바꿀 값을 입력하세요: ");
-                    scanf_s("%d", &newData);
-                    WriteProcessMemory(hProcess, pTargetPos, &newData, 4, NULL);
-                    printf("\n데이터를 바꿨습니다! 탐색을 더 진행합니다.\n");
-                }
-                ptr += 4;
-                counted += 4;
-            }          
+                int newData;
+                printf("\n데이터를 찾았습니다! 바꿀 값을 입력하세요: ");
+                scanf_s("%d", &newData);
+                WriteProcessMemory(hProcess, pTargetPos, &newData, 4, NULL);
+                printf("\n데이터를 바꿨습니다! 탐색을 더 진행합니다.");
+            }  
+
+            free(buffer);
         }
         pMemoryPosOld = pMemoryPos;
         pMemoryPos += size;
     }
+
     if(pTargetPos == nullptr)
-        printf("해당 데이터를 찾지 못했습니다!\n");
-    printf("데이터 탐색이 끝났습니다.\n");
+        printf("\n해당 데이터를 찾지 못했습니다!");
+    printf("\n데이터 탐색이 끝났습니다.\n");
 }
 
 int main()
